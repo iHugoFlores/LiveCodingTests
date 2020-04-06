659479c3aab2a4278ed9936224a969a5422aa237
Hugo Flores <hfp.ing2@gmail.com>
2020-04-06 12:34:41 -0400

    Base project

---
+++ LiveCodingTest/Challenges.swift
@@ -0,0 +1,13 @@
+//
+//  Challenges.swift
+//  LiveCodingTest
+//
+//  Created by Field Employee on 4/6/20.
+//  Copyright © 2020 Hugo Flores. All rights reserved.
+//
+
+import Foundation
+
+class Challenges {
+    
+}
---
+++ LiveCodingTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LiveCodingTest
+//
+//  Created by Field Employee on 4/6/20.
+//  Copyright © 2020 Hugo Flores. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
